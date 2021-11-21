059309b60b6ff9acf229948b34cc3f7f990bd59d
Michael Mao <jmao@gatech.edu>
2021-11-20 20:44:44 -0500

    updated sidebar

--- new_app.py
+++ new_app.py
@@ -8,28 +8,38 @@ import numpy as np
 st.set_page_config(page_title='Determining Feature Importance - Machine Learning App',
                    layout='wide')
 
-
 ######################################
-## Page Title 
+## Page Title
 ######################################
-st.title("Deep Neural Network Performance Optimization using GridSearch and GTDLBench")
-st.write("**CS 6220 Project Group 18:** Junyan Mao, Jintong Jiang, Yiqiong Xiao, Gabriel Wang, Andrew Wang")
+st.title('Deep Neural Network Performance Optimization using GridSearch and GTDLBench')
+st.write('**CS 6220 Project Group 18:** Junyan Mao, Jintong Jiang, Yiqiong Xiao, Gabriel Wang, Andrew Wang')
 
 ######################################
 ## App Workflow Process
 ######################################
-st.header("Process")
-st.write("1. Upload dataset or use sample datasets")
-st.write("2. Utilize GridSearch & GTDLBench to run ")
-
+st.header('Process')
+st.write('1. Upload dataset or use sample datasets')
+st.write('2. Utilize GridSearch & GTDLBench to run ')
 
 ######################################
 ## Sidebar
 ######################################
-# Input your csv
-st.sidebar.header('Upload your CSV data')
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-st.sidebar.markdown("""
-[Example CSV input file]
-()
-""")
+
+# Step 1: select dataset
+st.sidebar.header('Step 1: Pick your dataset')
+# upload file
+uploaded_file = st.sidebar.file_uploader('Upload your own data', type=['csv'])
+st.sidebar.markdown('[Example CSV input file]()')
+
+# select dataset
+dataset_select = st.sidebar.selectbox(
+    'Or, pick an example dataset from the list',
+    ('CIFAR-10', 'Fashion-MNIST', 'MNIST', 'ImageNet')
+)
+
+# Step 2: select model
+st.sidebar.header('Step 2: Pick your model')
+model_select = st.sidebar.selectbox(
+    'Pick a model',
+    ('AlexNet', 'ResNet-18', 'VGG-16')
+)
