2dc245b1a825078675e7d8149cf2409947d59629
Michael Mao <jmao@gatech.edu>
2021-11-21 22:47:09 -0500

    add accuracy plot

--- new_app.py
+++ new_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+import torch
 import train
 import utils
 
+from torch import nn
+from torch import optim
 from torchvision.utils import make_grid
 
 # Page layout
@@ -66,3 +68,107 @@ if st.sidebar.button('Generate'):
             images, labels = data_iter.next()
             sample_image = utils.convert_tensor_for_display(make_grid(images))
             st.image(sample_image)
+
+        st.subheader('Training:')
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        epochs = 5
+        patience = 3
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.classifier.parameters())
+        test_loss = []
+        train_loss = []
+        test_acc = []
+        train_acc = []
+        epoch_no_improve = 0
+        prev_test_loss = None
+        progress_bar = st.progress(0)
+        for epoch in range(epochs):
+            progress_bar.progress((epoch + 1) * 20)
+            for data, targets in iter(train_loader):
+                data = data.to(device)
+                targets = targets.to(device)
+
+                results = model.forward(data)
+                # Computes loss
+                loss = criterion(results, targets)
+
+                # Updates model
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            # Evaluate after every epoch
+            test_loss_val = 0
+            test_accuracy = 0
+            train_loss_val = 0
+            train_accuracy = 0
+            model.eval()
+            with torch.no_grad():
+                for inputs, labels in train_loader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    scores = model.forward(inputs)
+                    batch_loss = criterion(scores, labels)
+                    train_loss_val += batch_loss.item()
+
+                    # Calculate accuracy
+                    top_p, top_class = scores.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    train_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+                for inputs, labels in test_loader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    scores = model.forward(inputs)
+                    batch_loss = criterion(scores, labels)
+                    test_loss_val += batch_loss.item()
+
+                    # Calculate accuracy
+                    top_p, top_class = scores.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    test_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+            final_test_loss = test_loss_val/len(test_loader)
+            final_train_loss = train_loss_val/len(train_loader)
+            final_test_acc = test_accuracy/len(test_loader)
+            final_train_acc = train_accuracy/len(train_loader)
+
+            test_loss.append(final_test_loss)
+            train_loss.append(final_train_loss)
+            test_acc.append(final_test_acc)
+            train_acc.append(final_train_acc)
+
+            st.write(f"Epoch {epoch+1}/{epochs}.. "
+                    f"Train loss: {final_train_loss:.3f}.. "
+                    f"Test loss: {final_test_loss:.3f}.. "
+                    f"Train accuracy: {final_train_acc:.3f}.. "
+                    f"Test accuracy: {final_test_acc:.3f}")
+
+            if prev_test_loss:
+                # Check early stopping
+                if prev_test_loss < final_test_loss:
+                    epoch_no_improve += 1
+                else:
+                    epoch_no_improve = 0
+
+            prev_test_loss = test_loss[-1]
+
+            if epoch_no_improve >= patience:
+                print("EARLY STOPPING")
+                break
+
+            model.train()
+
+        model.eval()
+
+        fig, ax = plt.subplots()
+        ax.plot(range(1, epochs + 1), train_acc, 'g', label='Train Accuracy')
+        ax.plot(range(1, epochs + 1), test_acc, 'b', label='Test Accuracy')
+        ax.set_title('Train and Test Loss')
+        ax.set_xlabel('Epochs')
+        ax.set_ylabel('Accuracy')
+        ax.legend()
+
+        st.pyplot(fig)
+
+        # plot_losses(epochs, train_loss, test_loss)
+        # plot_acc(epochs, train_acc, test_acc)
